b1ce03b8a5cb9a5eda51150d6131144a77dbf048
Thomas Kohlbacher <89933898+k0hlii@users.noreply.github.com>
2023-04-23 21:30:43 +0200

    The first version of the main-page

--- src/main/java/com/example/controllsdemo/HelloController.java
+++ src/main/java/com/example/controllsdemo/HelloController.java
@@ -15,78 +15,63 @@ import model.PersonDAO;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class HelloController  {
 
-    private SimpleListProperty listProperty = new SimpleListProperty();
-
-    private ObservableList<Person> persons;
-
-    @FXML
-    private ComboBox<Person> cbPersons;
-    @FXML
-    private TextField tfFirstName;
-    @FXML
-    private TextField tfLastName;
-    @FXML
-    private Button btnSave;
-    @FXML
-    private MenuItem miEdit;
-    @FXML
-    private Spinner<Integer> spYob;
-    @FXML
-    private ComboBox<Country> cbCountrys;
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        persons = PersonDAO.getPersons();
-
-        cbPersons.itemsProperty().bind(listProperty);
-        listProperty.setValue(persons);
-
-        spYob.setValueFactory(
-                new SpinnerValueFactory.IntegerSpinnerValueFactory(1958,2005)
-       );
-
-        cbCountrys.setItems(CountryDAO.getCountrys());
-    }
-
-    @FXML
-    public void handleCbPersonsAction(ActionEvent actionEvent) {
-        Person p = cbPersons.getSelectionModel().getSelectedItem();
-        System.out.println(p.getId() +" "+ p);
-    }
-
-    @FXML
-    public void handleMiEditAction(ActionEvent actionEvent) {
-        Person p = cbPersons.getSelectionModel().getSelectedItem();
-        if (p == null)
-            return;
-
-        tfFirstName.setText(p.getFirstname());
-        tfLastName.setText(p.getLastname());
-        spYob.getValueFactory().setValue(p.getBirthyear());
-
-//        for (Toggle tg: tgCountrys.getToggles()) {
-//            if (((RadioButton)tg).getText().equals(p.getCountry()))
-//                tgCountrys.selectToggle(tg);
-//        }
-    }
-
-    @FXML
-    public void handleBtnSaveAction(ActionEvent actionEvent) {
-        Person p = cbPersons.getSelectionModel().getSelectedItem();
-
-        if (p ==null)
-            return;
-
-        p.setFirstname(tfFirstName.getText());
-        p.setLastname(tfLastName.getText());
-        p.setBirthyear((Integer) spYob.getValue());
-        //p.setCountry(((RadioButton)tgCountrys.getSelectedToggle()).getText());
-
-        int x = cbPersons.getSelectionModel().getSelectedIndex();
-        persons.set(x,p);
-        PersonDAO.update(p);
-    }
+//    private SimpleListProperty listProperty = new SimpleListProperty();
+//
+//    private ObservableList<Person> persons;
+//
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        persons = PersonDAO.getPersons();
+//
+//        cbPersons.itemsProperty().bind(listProperty);
+//        listProperty.setValue(persons);
+//
+//        spYob.setValueFactory(
+//                new SpinnerValueFactory.IntegerSpinnerValueFactory(1958,2005)
+//       );
+//
+//        cbCountrys.setItems(CountryDAO.getCountrys());
+//    }
+//
+//    @Deprecated
+//    public void handleCbPersonsAction(ActionEvent actionEvent) {
+//        Person p = cbPersons.getSelectionModel().getSelectedItem();
+//        System.out.println(p.getId() +" "+ p);
+//    }
+//
+//    @Deprecated
+//    public void handleMiEditAction(ActionEvent actionEvent) {
+//        Person p = cbPersons.getSelectionModel().getSelectedItem();
+//        if (p == null)
+//            return;
+//
+//        tfFirstName.setText(p.getFirstname());
+//        tfLastName.setText(p.getLastname());
+//        spYob.getValueFactory().setValue(p.getBirthyear());
+//
+////        for (Toggle tg: tgCountrys.getToggles()) {
+////            if (((RadioButton)tg).getText().equals(p.getCountry()))
+////                tgCountrys.selectToggle(tg);
+////        }
+//    }
+//
+//    @Deprecated
+//    public void handleBtnSaveAction(ActionEvent actionEvent) {
+//        Person p = cbPersons.getSelectionModel().getSelectedItem();
+//
+//        if (p ==null)
+//            return;
+//
+//        p.setFirstname(tfFirstName.getText());
+//        p.setLastname(tfLastName.getText());
+//        p.setBirthyear((Integer) spYob.getValue());
+//        //p.setCountry(((RadioButton)tgCountrys.getSelectedToggle()).getText());
+//
+//        int x = cbPersons.getSelectionModel().getSelectedIndex();
+//        persons.set(x,p);
+//        PersonDAO.update(p);
+//    }
 }
