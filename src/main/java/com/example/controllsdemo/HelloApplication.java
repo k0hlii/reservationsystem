b0693cc1f8000b681e44a36c49d66a0bf9313445
Thomas Kohlbacher <89933898+k0hlii@users.noreply.github.com>
2023-04-25 20:42:20 +0200

    added css

--- src/main/java/com/example/controllsdemo/HelloApplication.java
+++ src/main/java/com/example/controllsdemo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Reservation System");
         stage.setScene(scene);
         stage.show();
     }
