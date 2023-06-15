1d0c854be0e22309e3ee4f78836dbe2461602888
Thomas Kohlbacher <89933898+k0hlii@users.noreply.github.com>
2023-06-15 23:49:22 +0200

    fixed a bug where the Insert for the Reservation into the DB did not execute

--- src/main/java/model/ReservationDAO.java
+++ src/main/java/model/ReservationDAO.java
@@ -98,6 +98,8 @@ public class ReservationDAO {
             preparedStatement.setInt(7, r.cusomerID);
             preparedStatement.setString(8, "reserved");
 
+            preparedStatement.executeUpdate();
+
         } catch (SQLException ex) {
             System.err.println("Error executing SQL: " + ex.getMessage());
         }
